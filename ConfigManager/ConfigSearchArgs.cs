1f3adda48097772811cfcb5c54951fe39e1cffad
batesonmatt <batesonmatt@yahoo.com>
2024-03-15 14:53:53 -0500

    Added ConfigSearchArgs for the search process.

---
+++ ConfigManager/ConfigSearchArgs.cs
@@ -0,0 +1,29 @@
+﻿namespace ConfigManager
+{
+    public class ConfigSearchArgs
+    {
+        #region Properties
+
+        public PluginType Plugin { get; }
+
+        public DateRangeType DateRange { get; }
+
+        #endregion
+
+        #region Constructors
+
+        public ConfigSearchArgs()
+        {
+            Plugin = PluginType.All;
+            DateRange = DateRangeType.AllTime;
+        }
+
+        public ConfigSearchArgs(PluginType plugin, DateRangeType dateRange)
+        {
+            Plugin = plugin;
+            DateRange = dateRange;
+        }
+
+        #endregion
+    }
+}

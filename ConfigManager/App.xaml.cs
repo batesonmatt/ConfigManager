4edf84992d54be759f42ae4d7d67496dc70cfed7
batesonmatt <batesonmatt@yahoo.com>
2024-03-16 12:47:02 -0500

    Added Login window to show as a dialog before the MainWindow.

--- ConfigManager/App.xaml.cs
+++ ConfigManager/App.xaml.cs
@@ -8,10 +8,42 @@ using System.Windows;
 
 namespace ConfigManager
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
     public partial class App : Application
     {
+        public void ApplicationStart(object sender, StartupEventArgs e)
+        {
+            try
+            {
+                //Disable shutdown when the dialog closes
+                Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
+
+                Login loginWindow = new();
+
+                if (loginWindow.ShowDialog() == true)
+                {
+                    MainWindow mainWindow = new(/*dialog.Data*/);
+
+                    //Re-enable normal shutdown mode.
+                    Current.ShutdownMode = ShutdownMode.OnMainWindowClose;
+                    Current.MainWindow = mainWindow;
+                    mainWindow.Show();
+                }
+                else
+                {
+                    Current.Shutdown(-1);
+                }
+            }
+            catch (Exception ex)
+            {
+                string message = $"Could not load user data.\n\nDetails:\n\n{ex.Message}";
+
+                if (ex.StackTrace is not null)
+                {
+                    message += $"\n\n{ex.StackTrace}";
+                }
+
+                MessageBox.Show(message, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
     }
 }
---
+++ ConfigManager/Login.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ConfigManager
+{
+    public partial class Login : Window
+    {
+        public Login()
+        {
+            InitializeComponent();
+
+            userTextBox.Text = GetCurrentUserName();
+
+            if (string.IsNullOrWhiteSpace(userTextBox.Text))
+            {
+                userTextBox.Focus();
+            }
+            else
+            {
+                passwordBox.Focus();
+            }
+        }
+
+        private static string GetCurrentUserName()
+        {
+            string username;
+
+            try
+            {
+                username = Environment.UserName;
+            }
+            catch
+            {
+                username = string.Empty;
+            }
+
+            return username;
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = false;
+        }
+    }
+}

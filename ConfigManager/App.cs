8392394e7f5f9d1dec60714ccd16a7f8ae781f3c
batesonmatt <batesonmatt@yahoo.com>
2024-03-16 17:43:12 -0500

    Added StoreDB and ActiveDirectoryUser singleton properties to App.

---
+++ ConfigManager/App.cs
@@ -0,0 +1,16 @@
+﻿using ConfigManager.DataLayer;
+using ConfigManager.Security;
+using System.Windows;
+
+namespace ConfigManager
+{
+    public partial class App : Application
+    {
+        #region Properties
+
+        public static StoreDB StoreDB { get; } = new();
+        public static ActiveDirectoryUser ActiveDirectoryUser { get; } = new();
+
+        #endregion
+    }
+}

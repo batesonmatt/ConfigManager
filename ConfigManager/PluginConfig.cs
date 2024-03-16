e44711478fb8ae72c41f350f786f368b927bd7a4
batesonmatt <batesonmatt@yahoo.com>
2024-03-16 00:51:12 -0500

    Added PluginConfig to encapsulate a particular plugin name and config file for that plugin.

---
+++ ConfigManager/PluginConfig.cs
@@ -0,0 +1,22 @@
+﻿namespace ConfigManager
+{
+    public class PluginConfig
+    {
+        #region Properties
+
+        public string PluginName { get; }
+        public string ConfigFile {  get; }
+
+        #endregion
+
+        #region Constructors
+
+        public PluginConfig(string pluginName, string configFile)
+        {
+            PluginName = string.IsNullOrWhiteSpace(pluginName) ? string.Empty : pluginName;
+            ConfigFile = string.IsNullOrWhiteSpace(configFile) ? string.Empty : configFile;
+        }
+
+        #endregion
+    }
+}

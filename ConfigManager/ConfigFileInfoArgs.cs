60697947cf21e48ccdc605fc865b02984ea3467e
batesonmatt <batesonmatt@yahoo.com>
2024-03-18 15:55:51 -0500

    Added ConfigFileInfoArgs to encapsulate FileInfo objects for a particular local file and it's live (deployed) version.

---
+++ ConfigManager/ConfigFileInfoArgs.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+
+namespace ConfigManager
+{
+    public class ConfigFileInfoArgs
+    {
+        #region Properties
+
+        public FileInfo LocalFile { get; } = null!;
+        public FileInfo LiveFile { get; } = null!;
+
+        #endregion
+
+        #region Constructors
+
+        public ConfigFileInfoArgs(FileInfo localFile)
+        {
+            LocalFile = localFile;
+        }
+
+        public ConfigFileInfoArgs(FileInfo localFile, FileInfo liveFile)
+        {
+            LocalFile = localFile;
+            LiveFile = liveFile;
+        }
+
+        #endregion
+    }
+}

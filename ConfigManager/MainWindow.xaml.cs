d887501ff08bb18f842dbf5f4d4743eff3e5268a
batesonmatt <batesonmatt@yahoo.com>
2024-03-21 08:24:01 -0500

    Removed unnecessary using statements.

--- ConfigManager/MainWindow.xaml.cs
+++ ConfigManager/MainWindow.xaml.cs
@@ -2,18 +2,8 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 
 namespace ConfigManager
 {

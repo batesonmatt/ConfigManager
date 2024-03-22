d49ac7a5b9079262afef274fa85ae6b83d73ea15
batesonmatt <batesonmatt@yahoo.com>
2024-03-22 16:20:22 -0500

    Added ConfigStatusArgs.Action property to represent the recommended action to take for a particular config file.

--- ConfigManager/ConfigStatusArgs.cs
+++ ConfigManager/ConfigStatusArgs.cs
@@ -6,15 +6,21 @@
 
         public int Id { get; }
         public string Status { get; } = string.Empty;
+        public string Action { get; } = string.Empty;
 
         #endregion
 
         #region Constructors
 
         public ConfigStatusArgs(int id, string status)
+            : this(id, status, string.Empty)
+        { }
+
+        public ConfigStatusArgs(int id, string status, string action)
         {
             Id = id;
             Status = string.IsNullOrWhiteSpace(status) ? string.Empty : status;
+            Action = string.IsNullOrWhiteSpace(action) ? string.Empty : action;
         }
 
         #endregion

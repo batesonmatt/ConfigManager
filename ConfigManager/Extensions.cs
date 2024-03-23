725a87cbd40c8c14769a73da3a7eefb13cfbebda
batesonmatt <batesonmatt@yahoo.com>
2024-03-23 17:11:22 -0500

    Added Extensions class with FileInfo.CompareModified extension method.

---
+++ ConfigManager/Extensions.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+
+namespace ConfigManager
+{
+    public static class Extensions
+    {
+        private const int READ_BYTES = sizeof(long);
+
+        public static int CompareModified(this FileInfo file, FileInfo other)
+        {
+            _ = file ?? throw new ArgumentNullException(nameof(file));
+            _ = other ?? throw new ArgumentNullException(nameof(other));
+
+            bool fileExists = file.Exists;
+            bool otherExists = other.Exists;
+
+            switch (fileExists)
+            {
+                case false when !otherExists:
+                    return 0;
+                case true when !otherExists:
+                    return 1;
+                case false when otherExists:
+                    return -1;
+                default:
+                    break;
+            }
+
+            // Same file.
+            if (file.FullName.Equals(other.FullName, StringComparison.OrdinalIgnoreCase))
+            {
+                return 0;
+            }
+
+            long fileModified = file.LastWriteTime.Ticks;
+            long otherModified = other.LastWriteTime.Ticks;
+
+            if (fileModified == otherModified)
+            {
+                return 0;
+            }
+
+            long fileLength = file.Length;
+            long otherLength = other.Length;
+
+            if (fileLength != otherLength)
+            {
+                if (fileModified > otherModified)
+                {
+                    return 1;
+                }
+
+                if (otherModified > fileModified)
+                {
+                    return -1;
+                }
+            }
+
+            int iterations = (int)Math.Ceiling((double)fileLength / READ_BYTES);
+            int i = 0;
+            bool filesAreEqual = true;
+
+            byte[] bytes1 = new byte[READ_BYTES];
+            byte[] bytes2 = new byte[READ_BYTES];
+
+            using FileStream stream1 = file.OpenRead();
+            using FileStream stream2 = other.OpenRead();
+
+            while (filesAreEqual && i < iterations)
+            {
+                stream1.Read(bytes1, 0, READ_BYTES);
+                stream2.Read(bytes2, 0, READ_BYTES);
+
+                if (BitConverter.ToInt64(bytes1, 0) != BitConverter.ToInt64(bytes2, 0))
+                {
+                    filesAreEqual = false;
+                }
+
+                i++;
+            }
+
+            if (filesAreEqual)
+            {
+                return 0;
+            }
+
+            return (fileModified > otherModified) ? 1 : -1;
+        }
+    }
+}

8fda036bfb8ead57bda8d96dc0dd9a9bea8adad0
batesonmatt <batesonmatt@yahoo.com>
2024-03-24 15:40:03 -0500

    Added ConfigEditorArgs to encapsulate the deploying file Id, path, and content.

---
+++ ConfigManager/ConfigEditorArgs.cs
@@ -0,0 +1,46 @@
+﻿namespace ConfigManager
+{
+    public class ConfigEditorArgs
+    {
+        #region Properties
+
+        public int Id { get; }
+        public string Path { get; } = string.Empty;
+        public string Content => _content;
+
+        #endregion
+
+        #region Fields
+
+        private string _content;
+
+        #endregion
+
+        #region Constructors
+
+        public ConfigEditorArgs(int id, string path, string content)
+        {
+            Id = id;
+            Path = string.IsNullOrWhiteSpace(path) ? string.Empty : path;
+            _content = string.IsNullOrWhiteSpace(content) ? string.Empty : content;
+        }
+
+        #endregion
+
+        #region Methods
+
+        public void Overwrite(string content)
+        {
+            if (string.IsNullOrWhiteSpace(content))
+            {
+                _content = string.Empty;
+            }
+            else
+            {
+                _content = content;
+            }
+        }
+
+        #endregion
+    }
+}
